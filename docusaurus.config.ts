616aebe45651e0a2f137c664e2248b9317419cac
Fernando Mejia <66635444+ferantoMSFT@users.noreply.github.com>
2025-04-24 15:42:00 +0000

    implemented zoom plugin

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,9 +72,22 @@ const config: Config = {
 
     ],
   ],
-
+  plugins: [
+    'plugin-image-zoom'
+  ],
   themeConfig: {
     // Replace with your project's social card
+    imageZoom: {
+      // CSS selector to apply the plugin to, defaults to '.markdown img'
+      selector: '.markdown img',
+      // Optional medium-zoom options
+      // see: https://www.npmjs.com/package/medium-zoom#options
+      options: {
+        margin: 24,
+        background: '#808080',
+        scrollOffset: 0
+      },
+    },
     image: 'img/aks-labs-social-card.png',
     navbar: {
       title: 'Logic Apps Labs',
